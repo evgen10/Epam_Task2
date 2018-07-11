47efd43617cb3b6cbde7598c0d7db2ca9653d194
evgen10 <i.am.mr.jackson96@gmail.com>
2018-07-11 13:26:37 +0600

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static string enteredString = "";
+
+
+        static void Main(string[] args)
+        {
+            do
+            {
+
+                try
+                {                   
+
+                    EnterString();
+
+                    StringSeparator stringSeparator = new StringSeparator();               
+
+                    Console.WriteLine(@"The first symbol of your string is ""{0}"".  ", stringSeparator.GetFirstSymbol(enteredString));
+
+
+                }
+                catch (Exception e)
+                {
+                    PrintErrorMessage(e.Message);
+                }
+
+
+            } while (Exit());
+
+        }
+        
+
+        static void PrintErrorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void EnterString()
+        {
+
+            Console.WriteLine("\nEnter a string!");
+            enteredString = Console.ReadLine();
+
+        }
+
+        static bool Exit()
+        {
+            Console.WriteLine(@"Press any key to continue. Press ""ESC"" to exit." + "\n");
+
+            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+    }
+}
---
+++ Task1/StringSeparator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class StringSeparator
+    {
+        public char GetFirstSymbol(string sourceString)
+        {
+
+            if (sourceString == string.Empty)
+            {
+                throw new Exception("User entered an empty string!");
+            }
+
+            return sourceString[0];
+
+        }
+
+
+
+    }
+}
